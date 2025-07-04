702cce10e45d0404b28de733d404403a5f9c6b56
Jagroop Singh <108449470+jay-samra@users.noreply.github.com>
2025-07-03 23:06:05 -0700

    Added Top K Frequent Elements

--- Arrays&Hashing/ContainsDuplicate.py
+++ Arrays&Hashing/ContainsDuplicate.py
@@ -1,1 +1,34 @@
+class Solution2:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+         # nested for loop to compare each value
+         # nested loop must remain one index position ahead of i
+         # compare each value as loop progresses
 
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        
+        return False
+
+        # T.C. = O(n) * O(n) = O(n^2)
+        # S.C. = O(1)
+
+# Trying to optimize the solution using Hash Set
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+         # use hash set to keep track of known values
+         # if current index value exists in set, return True
+         # else return False
+
+        tracker = set()
+         # for loop to iterate through nums
+        for i in range(len(nums)):
+            if nums[i] in tracker:
+                return True
+            else:
+                tracker.add(nums[i])
+        return False
+        
+        # T.C. = O(n)
+        # S.C. = O(n)
